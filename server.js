61cc5e58b98b1178b671d4e3f18d80bc46ad0c89
MoDaORIF <david.mostoslavski@formation.orif.ch>
2025-01-17 16:50:18 +0100

    fix: read count.txt fix

--- server.js
+++ server.js
@@ -1,28 +1,27 @@
 const { readFileSync, writeFileSync } = require('fs');
-
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    const count = readFileSync('./count.txt');
-    console.log('count', count)
+    const count = readFileSync('./count.txt', 'utf-8').trim(); // Use 'utf-8' to get a string
+    console.log('count', count);
 
-    const newCount = parseInt(count) + 1
+    const newCount = parseInt(count, 10) + 1;
 
-    writeFileSync('./count.txt', newCount);
+    writeFileSync('./count.txt', newCount.toString());
 
     res.send(`
-    <!DOCTYPE html>
-    <html lang='eng'>
-    <head>
-        <meta charset="utf.8" />
-        <title> test </title>
-    </head>
-    <body>
-        <p>Page views : ${newCount}</p>
-    </body>
-    `)
-
+        <!DOCTYPE html>
+        <html lang='en'>
+        <head>
+            <meta charset="utf-8" />
+            <title>Test</title>
+        </head>
+        <body>
+            <p>Page views: ${newCount}</p>
+        </body>
+        </html>
+    `);
 });
 
 app.listen(5000, () => console.log('http://localhost:5000/'));
